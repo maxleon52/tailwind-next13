6d41c6e0cf5d4a2b50ae93f0512f40d170a49bab
maxleon52 <maxleon522@gmail.com>
2023-09-02 17:42:57 -0300

    Apply composition pattern in Input

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="grid-cols-app grid min-h-screen border-4 border-red-600">
+        <div className="grid min-h-screen grid-cols-app">
           <Sidebar />
           <main className="px-4 pb-12 pt-8">{children}</main>
         </div>
---
+++ src/components/Input.tsx
@@ -0,0 +1,30 @@
+import { ComponentProps } from "react";
+
+type PrefixProps = ComponentProps<"div">;
+
+export function Prefix(props: PrefixProps) {
+  return <div {...props} />;
+}
+
+type ControlProps = ComponentProps<"input">;
+
+export function Control(props: ControlProps) {
+  return (
+    <input
+      type="text"
+      className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
+      {...props}
+    />
+  );
+}
+
+type RootProps = ComponentProps<"div">;
+
+export function Root(props: RootProps) {
+  return (
+    <div
+      className="flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+      {...props}
+    />
+  );
+}
