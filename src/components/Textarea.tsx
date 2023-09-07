8280f4e267878570d0c28a272ee7fdd01aee9904
maxleon52 <maxleon522@gmail.com>
2023-09-07 19:18:38 -0300

    add animattion effect focus

--- src/components/Textarea.tsx
+++ src/components/Textarea.tsx
@@ -7,7 +7,7 @@ export default function Textarea({ ...props }: TextareaProps) {
     <textarea
       {...props}
       id="bio"
-      className="min-h-[120px] w-full resize-y rounded-lg border border-zinc-300 px-3 py-2 shadow-sm"
+      className="min-h-[120px] w-full resize-y rounded-lg border border-zinc-300 px-3 py-2 shadow-sm outline-none focus:border-violet-300 focus:ring-4 focus:ring-violet-100"
       defaultValue="Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, architecto at? Odio corrupti, sed vero non, doloremque totam pariatur soluta sunt minus, ratione nesciunt eos culpa mollitia? Impedit, itaque vitae."
     />
   );
